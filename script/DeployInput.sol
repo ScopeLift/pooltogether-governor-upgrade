27c6fca4a59eafa0120ba2a8a5a69f3909319f05
keating <keating.dev@protonmail.com>
2023-07-10 13:39:53 -0400

    Add placeholder for the test directory

--- script/DeployInput.sol
+++ script/DeployInput.sol
@@ -5,5 +5,5 @@ contract DeployInput {
   // These are based on the current values for Governor Alpha
   uint256 constant INITIAL_VOTING_DELAY = 1;
   uint256 constant INITIAL_VOTING_PERIOD = 28_800;
-  uint256 constant INITIAL_PROPOSAL_THRESHOLD = 1_000e18;
+  uint256 constant INITIAL_PROPOSAL_THRESHOLD = 1000e18;
 }
