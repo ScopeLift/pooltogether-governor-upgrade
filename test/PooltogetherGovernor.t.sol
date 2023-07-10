6c438ef7b6a7e0eab961edada79bc6a6cf2127e1
keating <keating.dev@protonmail.com>
2023-07-10 19:50:43 -0400

    Setup test harness with initial test

---
+++ test/PooltogetherGovernor.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.18;
+
+import {PooltogetherGovernorTest} from "test/PooltogetherGovernorTest.sol";
+
+abstract contract BravoGovernorDeployTest is PooltogetherGovernorTest {
+  function testFuzz_deployment(uint256 _blockNumber) public {
+    assertEq(governorBravo.name(), "Pooltogether Governor Bravo");
+    assertEq(address(governorBravo.token()), POOL_TOKEN);
+    // These values were all copied directly from the mainnet alpha governor at:
+    //   0xDbD27635A534A3d3169Ef0498beB56Fb9c937489
+    assertEq(INITIAL_VOTING_DELAY, 1);
+    assertEq(INITIAL_VOTING_PERIOD, 28_800);
+    assertEq(INITIAL_PROPOSAL_THRESHOLD, 1_000e18);
+    // forgefmt: disable-end
+    assertEq(governorBravo.votingDelay(), INITIAL_VOTING_DELAY);
+    assertEq(governorBravo.votingPeriod(), INITIAL_VOTING_PERIOD);
+    assertEq(governorBravo.proposalThreshold(), INITIAL_PROPOSAL_THRESHOLD);
+    assertEq(governorBravo.quorum(_blockNumber), QUORUM);
+    assertEq(governorBravo.timelock(), TIMELOCK);
+    assertEq(governorBravo.COUNTING_MODE(), "support=bravo&quorum=for,abstain&params=fractional");
+  }
+}
+
+contract BravoGovernorDeployTestWithBravoDeployedByScript is BravoGovernorDeployTest {
+  function _useDeployedGovernorBravo() internal pure override returns (bool) {
+    return false;
+  }
+}
