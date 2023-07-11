e43db21c993b3a704c624e63c91b94af8c1f7064
keating <keating.dev@protonmail.com>
2023-07-11 12:17:49 -0400

    Change script name

--- script/Propose.s.sol
+++ script/Propose.s.sol
@@ -8,7 +8,7 @@ import {ICompoundTimelock} from
 import {PooltogetherGovernor} from "src/PooltogetherGovernor.sol";
 import {IGovernorAlpha} from "src/interfaces/IGovernorAlpha.sol";
 
-contract Deploy is Script {
+contract Propose is Script {
   IGovernorAlpha constant GOVERNOR_ALPHA =
     IGovernorAlpha(0xB3a87172F555ae2a2AB79Be60B336D2F7D0187f0);
   address constant PROPOSER = 0xe0e7b7C5aE92Fe94D2ae677D81214D6Ad7A11C27; // lonser.eth
