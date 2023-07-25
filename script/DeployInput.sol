946cfbf2701196bd206a230a0e2df708a8d7f14b
Ben DiFrancesco <ben@scopelift.co>
2023-07-25 17:18:03 -0400

    [review] Remove all contents to set up review PR

--- script/DeployInput.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.18;
-
-contract DeployInput {
-  // TODO Reach out to PoolTogether and update these numbers based on what they want.
-  uint256 constant INITIAL_VOTING_DELAY = 3600; // 12 hours
-  uint256 constant INITIAL_VOTING_PERIOD = 28_800;
-  uint256 constant INITIAL_PROPOSAL_THRESHOLD = 10_000e18;
-}
